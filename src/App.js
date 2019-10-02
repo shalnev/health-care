40a1d53c4bcdf460ad5d2509ae8496b51971a4b7
Shalnev <shalnevns@altarix.ru>
2019-10-02 06:44:26 +0400

    start footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import demoShortcutLogo from './demo.shortcut.svg';
+
+/* TODO: вёрстка
+1) написать хедер
+2) написать футер
+3) создать компонент
+ */
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__header">
+          header!!
       </header>
+      <main>
+          I'm main!
+      </main>
+      <footer className="App__footer footer">
+          <div className="footer__shortcut">
+              <img src={demoShortcutLogo} className="footer__icon" alt="demo shortcut logo" />
+              <span>Сегодня</span>
+          </div>
+          <div className="footer__shortcut">
+              <img src={demoShortcutLogo} className="footer__icon" alt="demo shortcut logo" />
+              <span>Статистика</span>
+          </div>
+      </footer>
     </div>
   );
 }
