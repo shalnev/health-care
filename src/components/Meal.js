7e030df5dfba2ccbbac66e68bb6ca305d5f1e185
Shalnev <shalnevns@altarix.ru>
2019-11-06 21:08:14 +0400

    WIP: creating base meal card

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './Meal.scss';
+import {HUNGER_STATES} from "../const";
+
+function RepeatableDropdown(props) {
+    return (
+        <select className={props.className}>
+            <option value='' disabled selected>{props.placeholder}</option>
+            {props.items.map(item => <option value={item}>{item}</option>)}
+        </select>
+    )
+}
 
 function Meal() {
+
+
     return (
         <div className="wrapper">
             <section className="header">
@@ -9,9 +21,17 @@ function Meal() {
                 <button className="header__button">Add</button>
             </section>
             <section className="card">
-                <button aria-label="add new record">
+                <button className="card__item" aria-label="add new record">
                     <span aria-label="plus" role="img">➕ add new record</span>
                 </button>
+                <RepeatableDropdown className="card__item" placeholder="Hungry" items={HUNGER_STATES}/>
+                <RepeatableDropdown className="card__item" placeholder="After meals" items={HUNGER_STATES}/>
+                <div className="card__item">
+                    <input name="time"/>
+                    <input name="date" />
+                </div>
+                <div className="card__item" name="location"/>
+                <textarea className="card__item" placeholder="feelings, party"/>
             </section>
         </div>
     );
